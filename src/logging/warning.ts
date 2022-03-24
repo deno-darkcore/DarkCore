245645aade15e30342521b7294312a9ad708603e
kokomi simp :3 <beol0127@gmail.com>
2022-03-24 18:31:33 -0400

    chore: update error -> log

--- src/logging/warning.ts
+++ src/logging/warning.ts
@@ -2,5 +2,5 @@ export function warn(msg: string): void {
   const orange = "\x1b[33m";
   const reset = "\x1b[0m";
 
-  console.error(`${orange}[WARNING]${reset}: ${msg}`)
+  console.log(`${orange}[WARNING]${reset}: ${msg}`)
 }
