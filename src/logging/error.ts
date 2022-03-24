3ac4370dc309a204114cb9560108e8cf0bf4cbbf
kokomi simp :3 <beol0127@gmail.com>
2022-03-24 18:23:44 -0400

    chore: update error syntax

--- src/logging/error.ts
+++ src/logging/error.ts
@@ -5,10 +5,10 @@
  * Logs an error message into the console
  */
 
-export function err(type: string, msg: string): void {
+export function error(type: string, msg: string): void {
   const red = "\x1b[31m";
   const reset = "\x1b[0m";
 
-  console.error(`${red}${type}${reset}: ${msg}`)
+  console.error(`${red}[ERROR] [${type}]${reset}: ${msg}`)
 }
 
