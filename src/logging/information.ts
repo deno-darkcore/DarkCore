f639024d79e145ff93c6a2192eb373de84a2775a
kokomi simp :3 <beol0127@gmail.com>
2022-03-24 18:31:08 -0400

    chore: update colours

--- src/logging/information.ts
+++ src/logging/information.ts
@@ -1,6 +1,6 @@
 export function info(msg: string): void {
-  const yellow = "\033[33m";
+  const blue = "\x1b[34m";
   const reset = "\x1b[0m";
 
-  console.log(`${yellow}[INFO]${reset}: ${msg}`);
+  console.log(`${blue}[INFO]${reset}: ${msg}`);
 }
