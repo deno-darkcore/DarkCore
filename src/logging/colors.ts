73815a1823e27aac55a01ef7761caace2657adcc
kokomi simp :3 <beol0127@gmail.com>
2022-03-25 21:34:35 -0400

    Update colors.ts

--- src/logging/colors.ts
+++ src/logging/colors.ts
@@ -1,12 +1,12 @@
 const colors = {
-  const black = "\x1b[30m";
-  const red = "\x1b[31m";
-  const yellow = "\x1b[33m";
-  const green = "\x1b[32m";
-  const blue = "\x1b[34m";
-  const purple = "\x1b[35m";
-  const cyan = "\x1b[36m";
-  const white = "\x1b[37m";
-  const reset = "\x1b[0m";
+  black = "\x1b[30m",
+  red = "\x1b[31m",
+  yellow = "\x1b[33m",
+  green = "\x1b[32m",
+  blue = "\x1b[34m",
+  purple = "\x1b[35m",
+  cyan = "\x1b[36m",
+  white = "\x1b[37m",
+  reset = "\x1b[0m"
 };
 export colors;
